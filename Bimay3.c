472b7fba58d4aaedbc7fa5c31eb3088523c0190f
TheophilusAE <103085737+TheophilusAE@users.noreply.github.com>
2023-09-21 11:03:56 +0700

    Create Bimay3.c

---
+++ Bimay3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main () {
+	int a,b,c;
+	
+	printf ("Enter first integer : \n");
+	scanf ("%d",&a);
+	
+	printf ("Enter second integer : \n");
+	scanf ("%d",&b);
+	
+	c = a + b;
+	
+	printf ("Sum is %d",c);
+	
+	return 0;
+}
